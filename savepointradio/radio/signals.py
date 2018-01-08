53437d6d96b403dfd2fd8f8fd30c7dad545a38cf
Josh Washburne <recursive.green@gmail.com>
2018-01-08 09:39:55 -0500

    I lied--now I added the docstring.

--- savepointradio/radio/signals.py
+++ savepointradio/radio/signals.py
@@ -10,6 +10,11 @@ from .models import Album, Artist, Game, Song
 @receiver(pre_save, sender=Game)
 @receiver(pre_save, sender=Song)
 def update_sorted_fields(sender, instance, **kwargs):
+    """
+    Whenever the name or title of a radio model object is created/modified, we
+    want to make sure we update the sorted field with the naturalized data for
+    sorting.
+    """
     if sender == Artist:
         instance.sorted_full_name = naturalize(instance.full_name)
     else:
