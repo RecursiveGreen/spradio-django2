f6e0365e6bb7ccee93903d71c963ec26252be452
Josh Washburne <recursive.green@gmail.com>
2018-03-30 12:53:41 -0400

    RadioProfile now has song request functionality.

---
+++ savepointradio/profiles/exceptions.py
@@ -0,0 +1,5 @@
+class MakeRequestError(Exception):
+    """
+    Exception raised when there is a problem making a song request.
+    """
+    pass
--- savepointradio/profiles/models.py
+++ savepointradio/profiles/models.py
@@ -4,12 +4,14 @@ from django.core.validators import (MaxLengthValidator, MinValueValidator,
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from core.behaviors import Timestampable
+from core.behaviors import Disableable, Timestampable
+from core.utils import get_setting
 from radio.models import Song
+from .exceptions import MakeRequestError
 from .managers import RequestManager
 
 
-class RadioProfile(Timestampable, models.Model):
+class RadioProfile(Disableable, Timestampable, models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE,
                                 null=True,
@@ -21,6 +23,55 @@ class RadioProfile(Timestampable, models.Model):
     song_requests = models.ManyToManyField(Song,
                                            related_name='song_requests',
                                            through='SongRequest')
+
+    def disable(self, reason=''):
+        super().disable(reason)
+        user = self.user
+        user.is_active = False
+        user.save(update_fields=['is_active'])
+
+    def enable(self):
+        super().enable()
+        user = self.user
+        user.is_active = True
+        user.save(update_fields=['is_active'])
+
+    def has_reached_request_max(self):
+        self_requests = SongRequest.music.unplayed().filter(profile=self)
+        max_requests = get_setting('max_song_requests')
+        return self_requests.count() >= max_requests
+
+    def can_request(self):
+        if not self.disabled:
+            return self.user.is_staff or not self.has_reached_request_max()
+        return False
+
+    def make_request(self, song_requested):
+        if isinstance(song_requested, int):
+            song = Song.objects.get(pk=song_requested)
+        else:
+            song = song_requested
+
+        if self.disabled:
+            raise MakeRequestError('User is currently disabled.')
+
+        if self.has_reached_request_max():
+            max_requests = get_setting('max_song_requests')
+            message = 'User has reached the maximum request limit ({}).'
+            raise MakeRequestError(message.format(max_requests))
+
+        if song.is_jingle and not self.user.is_staff:
+            raise MakeRequestError('Users cannot request a jingle.')
+
+        if song.is_song and not self.user.is_staff and not song.is_requestable:
+            if not song.is_enabled or not song.is_published:
+                raise MakeRequestError('Song not available at this time.')
+            play_again = song.get_date_when_requestable()
+            message = ('Song has been played recently and cannot be requested '
+                       'again until {}')
+            raise MakeRequestError(message.format(play_again))
+
+        SongRequest.objects.create(profile=self, song=song)
 
     def __str__(self):
         return "{}'s profile".format(self.user.get_username())
