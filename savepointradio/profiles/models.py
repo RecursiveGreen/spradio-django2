6f76b7c6113ef6de90fbdae95fe725f930739a77
RecursiveGreen <recursive.green@gmail.com>
2019-06-06 15:44:56 -0400

    ugettext is deprecated--use gettext instead.

--- savepointradio/profiles/models.py
+++ savepointradio/profiles/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.core.validators import (MaxLengthValidator, MinValueValidator,
                                     MaxValueValidator)
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from core.behaviors import Disableable, Timestampable
 from core.utils import get_setting
